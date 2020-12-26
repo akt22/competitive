92abfbf4e489f5e8d19b73b00f6c741a3c0b1d26
akt22 <akt.t.ksh.thy@gmail.com>
2020-12-26 23:45:50 +0900

    solved!!: pakencamp2019d

--- dp/pakencamp-2019-day3/d/main.py
+++ dp/pakencamp-2019-day3/d/main.py
@@ -1,12 +1,28 @@
 N = int(input())
 
-
-def pp(_flag):
-    for y in range(5):
-        for x in range(N):
-            print(_flag[y][x], end=" ")
-        print()
+colors = ["B", "W", "R"]
 
 
 flag = [list(input()) for _ in range(5)]
-pp(flag)
+
+INF = 10**9
+dp = [[INF] * (4) for _ in range(N + 1)]
+for j, color in enumerate(colors):
+    cnt = 5
+    for i in range(5):
+        if flag[i][0] == color:
+            cnt -= 1
+    dp[0][j] = cnt
+
+for i in range(1, N):
+    for j, color in enumerate(colors):
+        for k, color2 in enumerate(colors):
+            if color == color2:
+                continue
+            cnt = 5
+            for l in range(5):
+                if flag[l][i] == color:
+                    cnt -= 1
+            dp[i][j] = min(dp[i][j], dp[i - 1][k] + cnt)
+
+print(min(dp[N - 1]))
