7a5b5d69db39d2571f6ea2381ca10c4d77bef8e7
akt22 <akt.t.ksh.thy@gmail.com>
2021-01-07 21:30:29 +0900

    solved: arc032 b

---
+++ structure/arc032/b/main.py
@@ -0,0 +1,56 @@
+N, M = map(int, input().split())
+
+roads = [list(map(lambda x: int(x) - 1, input().split())) for _ in range(M)]
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.par = [-1] * n
+
+    def find(self, x):
+        if self.par[x] < 0:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        if self.par[x] > self.par[y]:
+            x, y = y, x
+        self.par[x] += self.par[y]
+        self.par[y] = x
+
+    def size(self, x):
+        return -self.par[self.find(x)]
+
+    def check(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.par) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+
+u = UnionFind(N)
+
+for a, b in roads:
+    u.union(a, b)
+
+ans = 0
+for p in range(1, N):
+    if u.check(0, p):
+        continue
+    u.union(0, p)
+    ans += 1
+print(ans)
