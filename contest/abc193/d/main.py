14cddf0cdda67a772d923380ee4c455716497af6
akt22 <akt.t.ksh.thy@gmail.com>
2021-02-28 11:15:28 +0900

    solved: abc193 c

--- contest/abc193/d/main.py
+++ contest/abc193/d/main.py
@@ -1,9 +1,12 @@
 from collections import Counter
 
-
 K = int(input())
 S = input()[:-1]
 T = input()[:-1]
+
+deck = Counter(dict(zip([str(i) for i in range(1, 10)], [K] * 10)))
+deck -= Counter(S)
+deck -= Counter(T)
 
 
 def f(cnt):
@@ -18,13 +21,19 @@ def f(cnt):
 
 cnt = 0
 for i in range(1, 10):
-    s = Counter(S + str(i))
+    i = str(i)
+    s = Counter(S + i)
     for j in range(1, 10):
-        t = Counter(T + str(j))
+        j = str(j)
+        t = Counter(T + j)
         c = s + t
         if any([v > K for v in c.values()]):
             continue
         if f(s) > f(t):
-            print(i, j)
-            cnt += 1
-print(cnt)
+            if i == j:
+                cnt += deck[i] * (deck[i] - 1)
+            else:
+                cnt += deck[i] * deck[j]
+
+total = sum(deck.values())
+print(float(cnt) / (total * (total - 1)))
