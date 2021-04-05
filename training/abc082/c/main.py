e003b4f03c66dc508271b87bdfe6d434b1a1478a
Ryo Akita <akt.t.ksh.thy@gmail.com>
2021-04-05 22:08:14 +0900

    solved: abc082 c

---
+++ training/abc082/c/main.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+N = int(input())
+A = list(map(int, input().split()))
+
+cnt = Counter(A)
+ans = 0
+for k, v in cnt.items():
+    if k > v:
+        ans += v
+    elif k < v:
+        ans += (v - k)
+print(ans)
