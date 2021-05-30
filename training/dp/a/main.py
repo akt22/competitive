bb048af5200c783ef939ee2f8317d71c9b46ecbb
Ryo Akita <akt.t.ksh.thy@gmail.com>
2021-05-30 22:09:45 +0900

    solved: dp a

--- training/dp/a/main.py
+++ training/dp/a/main.py
@@ -1,10 +1,12 @@
 N = int(input())
 H = list(map(int, input().split()))
 
-dp = [0] * (N + 1)
+INF = 1 << 60
+dp = [INF] * N
 dp[0] = 0
-dp[1] = abs(H[1] - H[0])
-for i in range(1, N - 1):
-    dp[i + 1] = dp[i - 1] + abs(H[i - 1] - H[i + 1])
-    dp[i + 1] = min(dp[i + 1], dp[i] + abs(H[i] - H[i + 1]))
+dp[1] = abs(H[0] - H[1])
+
+for i in range(N - 2):
+    dp[i + 1] = min(dp[i + 1], dp[i] + abs(H[i + 1] - H[i]))
+    dp[i + 2] = min(dp[i] + abs(H[i + 2] - H[i]), dp[i + 1] + abs(H[i + 2] - H[i + 1]))
 print(dp[N - 1])
