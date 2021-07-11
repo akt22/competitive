587643a097a1d4c541b65b70a45553f30d4e36c4
Ryo Akita <akt.t.ksh.thy@gmail.com>
2021-07-11 13:44:43 +0900

    dp f

--- review/dp/f/main.py
+++ review/dp/f/main.py
@@ -1,17 +1,20 @@
 s = input()
 t = input()
 
-dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+ls = len(s)
+lt = len(t)
 
-for i in range(len(s)):
-    for j in range(len(t)):
+dp = [[0] * (lt + 1) for _ in range(ls + 1)]
+
+for i in range(ls):
+    for j in range(lt):
         if s[i] == t[j]:
             dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + 1)
         dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i + 1][j])
         dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j + 1])
 
 ans = ""
-i, j = len(s), len(t)
+i, j = ls, lt
 while i > 0 and j > 0:
     if dp[i][j] == dp[i - 1][j]:
         i -= 1
