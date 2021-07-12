bba76495a78bc540dd7e94c5155ef9ec1dd508cb
akt22 <akt.t.ksh.thy@gmail.com>
2021-07-12 09:34:51 +0900

    retry dp f

--- review/dp/f/first.py
+++ review/dp/f/first.py
@@ -1,7 +1,8 @@
 s = input()
 t = input()
 
-ls, lt = len(s), len(t)
+ls = len(s)
+lt = len(t)
 
 dp = [[0] * (lt + 1) for _ in range(ls + 1)]
 
@@ -9,22 +10,17 @@ for i in range(ls):
     for j in range(lt):
         if s[i] == t[j]:
             dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + 1)
-        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j + 1])
         dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i + 1][j])
+        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j + 1])
 
-# TODO: 後でdpテーブルを描いて可視化
-print(dp)
 ans = ""
 i, j = ls, lt
 while i > 0 and j > 0:
-    print(f"(i, j) = ({i}, {j}) -> {dp[i][j]}")
-    # 既存のやつの場合はどこで出てるかを探索
     if dp[i][j] == dp[i - 1][j]:
         i -= 1
     elif dp[i][j] == dp[i][j - 1]:
         j -= 1
     else:
-        # 新しいやつが出たらそいつの時にインクリメントされたってこと
         ans += s[i - 1]
         i -= 1
         j -= 1
