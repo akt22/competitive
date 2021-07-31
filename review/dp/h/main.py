7ab708c4b2bf581192d77729a1ab489a89c8e65d
akt22 <akt.t.ksh.thy@gmail.com>
2021-07-31 12:11:07 +0900

    solved: dp h

--- review/dp/h/main.py
+++ review/dp/h/main.py
@@ -3,16 +3,22 @@ maze = []
 for i in range(H):
     maze.append(list(input()))
 
-moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
 dp = [[0] * W for _ in range(H)]
+for i in range(W):
+    if maze[0][i] == '#':
+        break
+    dp[0][i] = 1
+for j in range(H):
+    if maze[j][0] == '#':
+        break
+    dp[j][0] = 1
+
 MOD = 10**9 + 7
 
-dp[0][0] = 1
-
-for i in range(H):
-    for j in range(W):
-        if i + 1 < H and maze[i + 1][j] == '.':
-            dp[i + 1][j] = (dp[i + 1][j] + dp[i][j]) % MOD
-        if j + 1 < W and maze[i][j + 1] == ".":
-            dp[i][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD
+for i in range(1, H):
+    for j in range(1, W):
+        if maze[i][j] == '.':
+            dp[i][j] = dp[i][j] + dp[i - 1][j] + dp[i][j - 1]
+            dp[i][j] %= MOD
 print(dp[H - 1][W - 1])
