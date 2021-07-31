3efbce53530c0f57106a73d8f57e9312f59431a0
akt22 <akt.t.ksh.thy@gmail.com>
2021-07-31 16:35:15 +0900

    dp j

--- review/dp/j/main.py
+++ review/dp/j/main.py
@@ -1,32 +1,23 @@
 from collections import Counter
 
-import sys
-sys.setrecursionlimit(500000)
-
-
 N = int(input())
 A = list(map(int, input().split()))
 cnt = Counter(A)
 
-dp = [[[-1] * (N + 2) for _ in range(N + 2)] for _ in range(N + 2)]
+dp = [[[0.0] * (N + 2) for _ in range(N + 2)] for _ in range(N + 2)]
 
 
-def rec(i, j, k):
-    if dp[i][j][k] != -1:
-        return dp[i][j][k]
-    if i == 0 and j == 0 and k == 0:
-        return 0.0
-    ret = 0.0
-    if i > 0:
-        ret += i * rec(i - 1, j, k)
-    if j > 0:
-        ret += j * rec(i + 1, j - 1, k)
-    if k > 0:
-        ret += k * rec(i, j + 1, k - 1)
-    ret += N
-    ret *= 1 / (i + j + k)
-    dp[i][j][k] = ret
-    return ret
-
-
-print(rec(*[cnt[i] for i in range(1, 4)]))
+for k in range(N + 1):
+    for j in range(N + 1 - k):
+        for i in range(N + 1 - k - j):
+            if i == j == k == 0:
+                continue
+            if i > 0:
+                dp[i][j][k] += i * dp[i - 1][j][k]
+            if j > 0:
+                dp[i][j][k] += j * dp[i + 1][j - 1][k]
+            if k > 0:
+                dp[i][j][k] += k * dp[i][j + 1][k - 1]
+            dp[i][j][k] += N
+            dp[i][j][k] /= i + j + k
+print(dp[cnt[1]][cnt[2]][cnt[3]])
