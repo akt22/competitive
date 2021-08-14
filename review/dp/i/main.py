d9d88fa68e579371f0062b6fd46f3e40700cb53e
akt22 <akt.t.ksh.thy@gmail.com>
2021-08-14 23:20:26 +0900

    dp: i

---
+++ review/dp/i/main.py
@@ -0,0 +1,14 @@
+N = int(input())
+P = list(map(float, input().split()))
+
+dp = [[0.0] * (N + 1) for _ in range(N + 1)]
+dp[0][0] = 1.
+
+for i in range(N):
+    for j in range(i + 1):
+        dp[i + 1][j] += dp[i][j] * (1.0 - P[i])
+        dp[i + 1][j + 1] += dp[i][j] * P[i]
+ans = 0.0
+for i in range(N // 2 + 1, N + 1):
+    ans += dp[N][i]
+print(ans)
