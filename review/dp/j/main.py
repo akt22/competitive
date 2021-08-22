996cf4e017e2200f092a54274541acf19d906ab8
akt22 <akt.t.ksh.thy@gmail.com>
2021-08-22 17:59:06 +0900

    dp j

---
+++ review/dp/j/main.py
@@ -0,0 +1,21 @@
+from collections import Counter
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = [[[.0] * (N + 2) for _ in range(N + 2)] for _ in range(N + 2)]
+
+for k in range(N + 1):
+    for j in range(N + 1 - k):
+        for i in range(N + 1 - k - j):
+            if i == 0 and j == 0 and k == 0:
+                continue
+            if i > 0:
+                dp[i][j][k] += i * dp[i - 1][j][k]
+            if j > 0:
+                dp[i][j][k] += j * dp[i + 1][j - 1][k]
+            if k > 0:
+                dp[i][j][k] += k * dp[i][j + 1][k - 1]
+            dp[i][j][k] += N
+            dp[i][j][k] /= i + j + k
+cnt = Counter(A)
+print(dp[cnt[1]][cnt[2]][cnt[3]])
