e30f71369797a87acfac7e930e6400d4a6d31d0c
akt22 <akt.t.ksh.thy@gmail.com>
2022-09-02 00:15:30 +0900

    fix

--- training/abc262/b/main.py
+++ training/abc262/b/main.py
@@ -1,28 +1,17 @@
-from collections import defaultdict
 
 N, M = list(map(int, input().split()))
-g = defaultdict(list)
+
+g = [[False] * N for _ in range(N)]
 for _ in range(M):
     u, v = list(map(int, input().split()))
-    g[u].append(v)
-    g[v].append(u)
-
+    g[u - 1][v - 1] = True
+    g[v - 1][u - 1] = True
 
 ans = 0
 
-
-def dps(start, depth, current):
-    cnt = 0
-    if depth == 3 and start == current:
-        return 1
-    if depth == 3:
-        return 0
-    for i in g[current]:
-        cnt += dps(start, depth + 1, i)
-    return cnt
-
-
-for i in range(1, N + 1):
-    ans += dps(i, 0, i)
-
-print(ans // 6)
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            if g[i][j] and g[j][k] and g[k][i]:
+                ans += 1
+print(ans)
