63ecef991f4b2965f9c6d452dad4720b5c22d55a
akt22 <akt.t.ksh.thy@gmail.com>
2022-09-08 23:56:07 +0900

    solved: abc257 b

---
+++ training/abc257/b/main.py
@@ -0,0 +1,15 @@
+N, K, Q = list(map(int, input().split()))
+
+A = list(map(int, input().split()))
+L = list(map(int, input().split()))
+
+for l in L:
+    if A[l - 1] == N:
+        continue
+    if l == K:
+        A[l - 1] += 1
+        continue
+    if A[l] == A[l - 1] + 1:
+        continue
+    A[l - 1] += 1
+print(*A)
