77380be9fc99198a94abd2cda7f5080b121fdd8e
akt22 <akt.t.ksh.thy@gmail.com>
2022-09-16 00:15:54 +0900

    solved: abc250 a

---
+++ training/abc250/a/main.py
@@ -0,0 +1,13 @@
+H, W = list(map(int, input().split()))
+R, C = list(map(int, input().split()))
+
+ans = 0
+if R - 1 > 0:
+    ans += 1
+if R + 1 <= H:
+    ans += 1
+if C - 1 > 0:
+    ans += 1
+if C + 1 <= W:
+    ans += 1
+print(ans)
