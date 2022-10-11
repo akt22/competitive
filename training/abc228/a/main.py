66dfc5e968ec767e0b6c73324a22b75d45a0365b
akt22 <akt.t.ksh.thy@gmail.com>
2022-10-11 22:43:49 +0900

    solved: abc228 a

---
+++ training/abc228/a/main.py
@@ -0,0 +1,11 @@
+S, T, X = list(map(int, input().split()))
+if S < T:
+    if S <= X < T:
+        print('Yes')
+    else:
+        print('No')
+else:
+    if T <= X < S:
+        print('No')
+    else:
+        print('Yes')
