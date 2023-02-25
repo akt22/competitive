801d96f27541cb9b9aaa9020edb1e1b67be2106d
akt22 <akt.t.ksh.thy@gmail.com>
2023-02-25 12:33:29 +0900

    solved: abc288 a

---
+++ .archived/golang/abc288/a/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ===========================================================================================
+// init
+// -------------------------------------------------------------------------------------------
+const BUFSIZE = 10000000
+
+var rdr *bufio.Reader = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+
+// ===========================================================================================
+// utilities
+// -------------------------------------------------------------------------------------------
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readline() string {
+	line, _, err := rdr.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+	return string(line)
+}
+
+func mapAtoi(s string) []int {
+	ret := make([]int, 0)
+	if s == "" {
+		return ret
+	}
+
+	l := strings.Split(s, " ")
+	for _, elem := range l {
+		ret = append(ret, atoi(elem))
+	}
+	return ret
+}
+
+// ===========================================================================================
+// main
+// -------------------------------------------------------------------------------------------
+func main() {
+	N := atoi(readline())
+	lines := make([][]int, N)
+	for i := 0; i < N; i++ {
+		lines[i] = mapAtoi(readline())
+	}
+
+	for _, line := range lines {
+		fmt.Println(line[0] + line[1])
+	}
+}
