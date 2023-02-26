f1b2fff4b4558222a867ce553e495aa3551d1e44
akt22 <akt.t.ksh.thy@gmail.com>
2023-02-26 11:11:41 +0900

    solved: abc287 a

---
+++ .archived/golang/abc287/a/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ===========================================================================================
+// init
+// -------------------------------------------------------------------------------------------
+const BUFSIZE = 10000000
+
+var rdr *bufio.Reader = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+
+// ===========================================================================================
+// utilities
+// -------------------------------------------------------------------------------------------
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readline() string {
+	line, _, err := rdr.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+	return string(line)
+}
+
+func mapAtoi(s string) []int {
+	ret := make([]int, 0)
+	if s == "" {
+		return ret
+	}
+
+	l := strings.Fields(s)
+	for _, elem := range l {
+		ret = append(ret, atoi(elem))
+	}
+	return ret
+}
+
+// ===========================================================================================
+// main
+// -------------------------------------------------------------------------------------------
+func main() {
+	N := atoi(readline())
+	S := make([]string, N)
+	for i := 0; i < N; i++ {
+		S[i] = readline()
+	}
+
+	tmp := make(map[string]int)
+	for _, s := range S {
+		tmp[s]++
+	}
+	if tmp["For"] > tmp["Against"] {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
