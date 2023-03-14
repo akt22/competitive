b2f589bca978265849a1ba704cf2a1e5382b05d8
akt22 <akt.t.ksh.thy@gmail.com>
2023-03-14 10:58:55 +0900

    solved: abc293 a

---
+++ .archived/golang/abc293/a/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ===========================================================================================
+// init
+// -------------------------------------------------------------------------------------------
+const BUFSIZE = 10000000
+
+var rdr *bufio.Reader = bufio.NewReaderSize(os.Stdin, BUFSIZE)
+
+// ===========================================================================================
+// utilities
+// -------------------------------------------------------------------------------------------
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func readline() string {
+	line, _, err := rdr.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+	return string(line)
+}
+
+func mapAtoi(s string) []int {
+	ret := make([]int, 0)
+	if s == "" {
+		return ret
+	}
+
+	l := strings.Fields(s)
+	for _, elem := range l {
+		ret = append(ret, atoi(elem))
+	}
+	return ret
+}
+
+// ===========================================================================================
+// main
+// -------------------------------------------------------------------------------------------
+func main() {
+	S := readline()
+
+	s := ""
+	for i := 0; i < len(S)/2; i++ {
+		s += string(S[2*i+1]) + string(S[2*i])
+	}
+	fmt.Println(s)
+}
