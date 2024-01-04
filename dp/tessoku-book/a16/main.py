f8c5bd7c4b585c2830a964148397efbb9bd00f69
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-04 22:48:18 +0900

    fix

--- dp/tessoku-book/a16/main.py
+++ dp/tessoku-book/a16/main.py
@@ -2,7 +2,7 @@ N = int(input())
 A = [0, 0] + list(map(int, input().split()))
 B = [0, 0, 0] + list(map(int, input().split()))
 
-dp = [0] * (N+1)
+dp = [0] * (N + 1)
 dp[2] = A[2]
 
 for i in range(3, N + 1):
