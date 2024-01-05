3163276553420a100f9f27ee75e563d94b631895
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-05 21:58:33 +0900

    fix: a17

--- dp/tessoku-book/a17/main.py
+++ dp/tessoku-book/a17/main.py
@@ -5,25 +5,15 @@ B = [0, 0, 0] + list(map(int, input().split()))
 dp = [0] * (N + 1)
 dp[2] = A[2]
 
-step = [0, 0, 1]
-
 for i in range(3, N + 1):
-    left = dp[i - 1] + A[i]
-    right = dp[i - 2] + B[i]
-    if left < right:
-        step.append(i - 1)
-        dp[i] = left
-    else:
-        step.append(i - 2)
-        dp[i] = right
+    dp[i] = min(dp[i - 1] + A[i], dp[i - 2] + B[i])
 
 ans = [N]
+idx = N
 
-i = N
-while i > 1:
-    prev = step[i]
-    ans.append(prev)
-    i = prev
+while idx > 1:
+    idx = idx - 1 if dp[idx - 1] == dp[idx] - A[idx] else idx - 2
+    ans.append(idx)
 
 print(len(ans))
 ans.reverse()
