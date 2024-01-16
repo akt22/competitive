9f1707385362d8d8d6704bf040b6f493e3bf04a9
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-16 22:43:22 +0900

    solved: a16

--- dp/tessoku-book/a16/main.py
+++ dp/tessoku-book/a16/main.py
@@ -2,10 +2,9 @@ N = int(input())
 A = [0, 0] + list(map(int, input().split()))
 B = [0, 0, 0] + list(map(int, input().split()))
 
-dp = [0] * (N + 1)
+dp = [0] * (N+1)
 dp[2] = A[2]
 
-for i in range(3, N + 1):
+for i in range(3, N+1):
     dp[i] = min(dp[i-1] + A[i], dp[i-2] + B[i])
-
 print(dp[N])
