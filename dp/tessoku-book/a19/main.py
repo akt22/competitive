34fc42b4f33771868db3a2ce6e231ba74573e4e8
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-21 19:51:35 +0900

    solved: a19

--- dp/tessoku-book/a19/main.py
+++ dp/tessoku-book/a19/main.py
@@ -1,14 +1,18 @@
 N, W = list(map(int, input().split()))
-items = [[0, 0]] + [list(map(int, input().split())) for _ in range(N)]
+w, v = [0], [0]
+
+for _ in range(N):
+    _w, _v = list(map(int, input().split()))
+    w.append(_w)
+    v.append(_v)
 
 dp = [[0] * (W + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
-    for j in range(1, W + 1):
-        dp[i][j] = max(dp[i - 1][j], dp[i][j])
+    for j in range(W + 1):
+        if j - w[i] >= 0:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
+        else:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j])
 
-        w, v = items[i]
-        if j - w >= 0:
-            dp[i][j] = max(dp[i - 1][j - w] + v, dp[i][j])  # dp[i][j] は何度も通るので、maxで毎回比較
-
-print(max(dp[N]))
+print(max(*dp[N]))
