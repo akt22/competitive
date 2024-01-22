8cd3a81c68bfc3456148763942061c19be0df348
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-22 23:58:04 +0900

    solved: a20

--- dp/tessoku-book/a20/main.py
+++ dp/tessoku-book/a20/main.py
@@ -3,15 +3,11 @@ T = input()
 
 dp = [[0] * (len(T) + 1) for _ in range(len(S) + 1)]
 
-for i in range(1, len(S) + 1):
-    s = S[i - 1]
-    for j in range(1, len(T) + 1):
-        t = T[j - 1]
-        if s == t:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + 1)
+for i in range(len(S)):
+    for j in range(len(T)):
+        if S[i] == T[j]:
+            dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i - 1][j], dp[i][j - 1])
         else:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i][j - 1])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-# print(*dp, sep="\n")
-
-print(dp[len(S)][len(T)])
+print(dp[len(S) - 1][len(T) - 1])
