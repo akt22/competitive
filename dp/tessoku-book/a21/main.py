84fd63bee5995fb5e032993461403feac8856579
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-23 23:30:10 +0900

    a21

--- dp/tessoku-book/a21/main.py
+++ dp/tessoku-book/a21/main.py
@@ -9,27 +9,20 @@ A.append(0)
 
 dp = [[0] * (N + 1) for _ in range(N + 1)]
 
-# length = N-1 は dp[1][N] なのでいらない
 for length in range(N - 2, -1, -1):
     for l in range(1, N - length + 1):
         r = l + length
+        score1 = A[l - 1] if (l - 1) <= P[l - 1] <= r else 0
+        score2 = A[r + 1] if l <= P[r + 1] <= (r + 1) else 0
 
-        score1 = A[l - 1] if l <= P[l - 1] <= r else 0
-        score2 = A[r + 1] if l <= P[r + 1] <= r else 0
-
-        try:
-            if r == N:
-                dp[l][r] = dp[l - 1][r] + score1
-            elif l == 1:
-                dp[l][r] = dp[l][r + 1] + score2
-            else:
-                dp[l][r] = max(dp[l - 1][r] + score1, dp[l][r + 1] + score2)
-        except:
-            print(length, l, r)
-
-# print(*dp, sep="\n")
+        if r == N:
+            dp[l][r] = dp[l - 1][r] + score1
+        elif l == 1:
+            dp[l][r] = dp[l][r + 1] + score2
+        else:
+            dp[l][r] = max(dp[l - 1][r] + score1, dp[l][r + 1] + score2)
 
 ans = 0
-for r in dp:
-    ans = max(ans, *r)
+for row in dp:
+    ans = max(ans, *row)
 print(ans)
