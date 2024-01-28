a05aa563c7055f60ba6c83f59bd14ffac70b2f3d
akt22 <akt.t.ksh.thy@gmail.com>
2024-01-28 17:10:03 +0900

    dp a

---
+++ dp/dp/a/main.py
@@ -0,0 +1,10 @@
+N = int(input())
+h = [0] + list(map(int, input().split()))
+
+dp = [0] * (N + 1)
+dp[2] = abs(h[2] - h[1])
+
+for i in range(3, N + 1):
+    dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), dp[i - 2] + abs(h[i] - h[i - 2]))
+
+print(dp[N])
