c7c814b9378515adf19ed17c48cd85e61fa96c7b
akt22 <akt.t.ksh.thy@gmail.com>
2024-02-08 23:00:54 +0900

    g

--- dp/dp/g/main.py
+++ dp/dp/g/main.py
@@ -1,5 +1,9 @@
 from collections import defaultdict
 
+import sys
+
+sys.setrecursionlimit(500000)
+
 N, M = list(map(int, input().split()))
 g = defaultdict(list)
 
@@ -7,9 +11,19 @@ for _ in range(M):
     x, y = list(map(int, input().split()))
     g[x].append(y)
 
-dp = [0] * (N + 1)
-dp[1] = 0
+dp = [-1] * (N + 1)
 
-for i in range(2, N + 1):
-    for j in g[i]:
-        dp[i][j] = max(dp[i][j], dp[i][i] + 1)
+
+def rec(dep):
+    if dp[dep] != -1:
+        return dp[dep]
+    ret = 0
+    for dest in g[dep]:
+        ret = max(ret, rec(dest) + 1)
+    dp[dep] = ret
+    return ret
+
+
+for i in range(1, N + 1):
+    rec(i)
+print(max(dp))
