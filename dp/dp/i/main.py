86f39d518cc21a83d9ec964bf6ca496ecf35980c
akt22 <akt.t.ksh.thy@gmail.com>
2024-02-20 22:36:02 +0900

    i

--- dp/dp/i/main.py
+++ dp/dp/i/main.py
@@ -2,16 +2,13 @@ N = int(input())
 p = list(map(float, input().split()))
 
 dp = [[0.0] * (N + 1) for _ in range(N + 1)]
-
 dp[0][0] = 1.0
 
 for i in range(N):
-    for j in range(i + 1):
-        dp[i + 1][j + 1] += dp[i][j] * p[i]
-        dp[i + 1][j] += dp[i][j] * (1.0 - p[i])
+    pi = p[i]
+    for j in range(N):
+        dp[i + 1][j] += dp[i][j] * (1.0 - pi)
+        dp[i + 1][j + 1] += dp[i][j] * pi
 
-ans = 0.0
-for j in range(N // 2 + 1, N + 1):
-    ans += dp[N][j]
-
-print(ans)
+# Nが奇数なので、表がN ~ (N/2+1)枚出る時が求める確率
+print(sum(dp[N][N // 2 + 1 :]))
