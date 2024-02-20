0568a16ff9c4ad194e6f64400c0c095523c1a3a2
akt22 <akt.t.ksh.thy@gmail.com>
2024-02-20 22:49:00 +0900

    a

--- dp/dp/a/main.py
+++ dp/dp/a/main.py
@@ -5,6 +5,6 @@ dp = [0] * (N + 1)
 dp[2] = abs(h[2] - h[1])
 
 for i in range(3, N + 1):
-    dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), dp[i - 2] + abs(h[i] - h[i - 2]))
+    dp[i] = min(abs(h[i] - h[i - 1]) + dp[i - 1], abs(h[i] - h[i - 2]) + dp[i - 2])
 
 print(dp[N])
