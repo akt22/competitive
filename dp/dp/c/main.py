4b26c1a5a5c5b50ec6f3df94b6a57138937a395f
akt22 <akt.t.ksh.thy@gmail.com>
2024-02-22 23:30:21 +0900

    c

--- dp/dp/c/main.py
+++ dp/dp/c/main.py
@@ -1,13 +1,13 @@
 N = int(input())
 a, b, c = [0], [0], [0]
 
-for _ in range(N):
+for i in range(N):
     _a, _b, _c = list(map(int, input().split()))
     a.append(_a)
     b.append(_b)
     c.append(_c)
 
-dp = [[0] * (3) for _ in range(N + 1)]
+dp = [[0] * 3 for _ in range(N + 1)]
 dp[1][0] = a[1]
 dp[1][1] = b[1]
 dp[1][2] = c[1]
