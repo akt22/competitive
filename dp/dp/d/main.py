7fb9794fc9bb37d2c8ee07f902f0e2edfe8622bf
akt22 <akt.t.ksh.thy@gmail.com>
2024-02-23 17:22:59 +0900

    solved: d

--- dp/dp/d/main.py
+++ dp/dp/d/main.py
@@ -1,19 +1,17 @@
 N, W = list(map(int, input().split()))
-
 w, v = [0], [0]
 for _ in range(N):
     _w, _v = list(map(int, input().split()))
     w.append(_w)
     v.append(_v)
 
-
 dp = [[0] * (W + 1) for _ in range(N + 1)]
 
-for i in range(1, N + 1):
-    for j in range(1, W + 1):
+for i in range(N + 1):
+    for j in range(W + 1):
+        dp[i][j] = max(dp[i][j], dp[i - 1][j])
         if j - w[i] >= 0:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
-        else:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j])
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i])
 
+# print(*dp, sep="\n")
 print(max(dp[N]))
