bf48ae5aa2a66ac49e5915abda13158a45ab009e
akt22 <akt.t.ksh.thy@gmail.com>
2024-02-27 21:36:51 +0900

    f

--- dp/dp/f/main.py
+++ dp/dp/f/main.py
@@ -6,11 +6,9 @@ ls, lt = len(s), len(t)
 dp = [[0] * (lt + 1) for _ in range(ls + 1)]
 
 for i in range(1, ls + 1):
-    _s = s[i - 1]
     for j in range(1, lt + 1):
-        _t = t[j - 1]
-        if _s == _t:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + 1)
+        if s[i - 1] == t[j - 1]:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
         dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i][j - 1])
 
 ans = ""
@@ -20,7 +18,7 @@ while ls > 0 and lt > 0:
     elif dp[ls][lt] == dp[ls][lt - 1]:
         lt -= 1
     else:
-        ans = s[ls - 1] + ans
         ls -= 1
         lt -= 1
-print(ans)
+        ans += s[ls]
+print(ans[::-1])
