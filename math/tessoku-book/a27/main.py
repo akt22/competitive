84a9d39d7e3074b7ed7952513a56cd5996f3e34f
akt22 <akt.t.ksh.thy@gmail.com>
2024-03-21 15:34:00 +0900

    fix: handwritten gcd

--- math/tessoku-book/a27/main.py
+++ math/tessoku-book/a27/main.py
@@ -1,5 +1,13 @@
-from math import gcd
-
 A, B = list(map(int, input().split()))
+
+
+def gcd(a, b):
+    while a >= 1 and b >= 1:
+        if a >= b:
+            a %= b
+        else:
+            b %= a
+    return a if a != 0 else b
+
 
 print(gcd(A, B))
