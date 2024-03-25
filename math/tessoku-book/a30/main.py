bf414149383a26f2e33133466c82f54c3e7dbc13
akt22 <akt.t.ksh.thy@gmail.com>
2024-03-25 23:36:28 +0900

    a30

--- math/tessoku-book/a30/main.py
+++ math/tessoku-book/a30/main.py
@@ -1,29 +1,18 @@
-from operator import mul
-from functools import reduce
-
-
 n, r = list(map(int, input().split()))
-
-
-def comb(n, r, mod=None, repetition=False):
-    if repetition:
-        n = n + r - 1
-        r = r - 1
-    r = min(n - r, r)
-    if r == 0:
-        return 1
-    if mod is None:
-        over = reduce(mul, range(n, n - r, -1))
-        under = reduce(mul, range(1, r + 1))
-        return over // under
-    else:
-        x, y = 1, 1
-        for i in range(1, r + 1):
-            x = x * (n + 1 - i) % mod
-            y = y * i % mod
-        return x * pow(y, mod - 2, mod) % mod
-
 
 MOD = 10**9 + 7
 
-print(comb(n, r, MOD))
+a = 1
+for i in range(1, n + 1):
+    a *= i
+    a %= MOD
+
+b = 1
+for i in range(1, r + 1):
+    b *= i
+    b %= MOD
+for i in range(1, n - r + 1):
+    b *= i
+    b %= MOD
+
+print((a * pow(b, MOD - 2, MOD)) % MOD)
