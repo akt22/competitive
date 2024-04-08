35d941bbd3416d0b0aff04d4ddeebe5e77e927e2
akt22 <akt.t.ksh.thy@gmail.com>
2024-04-08 23:37:36 +0900

    a26

--- math/tessoku-book/a26/main.py
+++ math/tessoku-book/a26/main.py
@@ -4,23 +4,13 @@ Q = int(input())
 X = [int(input()) for _ in range(Q)]
 
 N = 300000
-deleted = [False] * 300009
+cache = [False] * 300001
 
-# O(N * sqrt(X))
-# def is_prime(n):
-#     for i in range(2, int(sqrt(n)) + 1):
-#         if i != 2 and i % 2 == 0:
-#             continue
-#         if n % i == 0:
-#             return False
-#     return True
-
-# O(N loglog N)
 for i in range(2, int(sqrt(N)) + 1):
-    if deleted[i]:
+    if cache[i]:
         continue
     for j in range(i * 2, N + 1, i):
-        deleted[j] = True
+        cache[j] = True
 
 for x in X:
-    print("Yes" if not deleted[x] else "No")
+    print("No" if cache[x] else "Yes")
